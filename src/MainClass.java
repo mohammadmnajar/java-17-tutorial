98ab9c798bfa69c14052f9d9cc55e882aca0c287
Mohammad Najar <mmotieian@yahoo.com>
2024-05-27 13:03:24 -0500

    basics of java

---
+++ src/MainClass.java
@@ -0,0 +1,23 @@
+public class MainClass {
+
+    public static void main(String[] args) {
+        // This is a comment. We use // for commenting a single line
+        int age = 30;
+        System.out.println(age);
+        double price = 19.99;
+        System.out.println(price);
+
+        // declaring multiple variables
+        int x = 5, y = 10, z = 15;
+
+        // operators
+        int result = x + y;
+        System.out.println("result: " + result);
+        boolean isLess = x < y;
+        System.out.println("isLess: " + isLess);
+
+        // an expression in Java
+        int total = (x + y) * 10;
+        System.out.println("total: " + total);
+    }
+}
